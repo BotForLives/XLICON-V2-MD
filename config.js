d7aeb1a232a1df33775d3c0415c390531968f72e
BotForLives <147728496+BotForLives@users.noreply.github.com>
2024-05-16 21:41:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,17 +6,17 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 global.owner = [
-  ['918360234087', 'WHITE', true], 
-  ['233208179431', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
-  ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
-  ['233533763772', 'Abraham Dwamena2', true],
-  ['233508488192', 'Abraham Dwamena3', true]
+  ['27767494368', 'AVE', true], 
+  ['27677130304', 'SKYPEDIA', true],
+  ['27630747080', '1v!sib1le St@r', true], 
+  ['27678368809', 'Thato🕷️🕸️ ⚡', true],
+  ['27796338857', 'Dikhuna🕸️'🕷️, true],
+  ['27665572886', 'Ave bot', true]
 ] //Number of owners
 
-global.mods = ['923184070915', '94789481495', '923184474176', '233531910067', '233508488192'] 
-global.prems = ['923184070915', '233208179431', '94789481495', '918360234087', '233508488192']
-global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087', '233508488192']
+global.mods = ['27767494368', '27630747080', '27677130304', '27796338857', '27678368809'] 
+global.prems = ['27767494368', '27677130304', '27677130304', '27796338857', '27678368809']
+global.allowed = ['27767494368', '27677130304', '27678368809', '27630747080', '27796338857']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = global.keysZens[Math.floor(global.keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,9 +50,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = 'AveBot'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
+global.packname = 'AVE TEAM' 
 global.author = '@XLICON-V2' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
